0705b849d3b2187e283a0b84f70a29b61f68dfca
Ben Obee <benobee@amazon.com>
2020-05-23 13:35:56 -0700

    adding image loader

--- src/common/util.js
+++ src/common/util.js
@@ -1,3 +1,12 @@
 export const toArray = (nodeList) => {
     return [].slice.call(nodeList);
 };
+
+// Load all images via Squarespace's Responsive ImageLoader
+export const loadAllImages = () => {
+    const images = document.querySelectorAll("img[data-src]" );
+
+    for (let i = 0; i < images.length; i++) {
+      ImageLoader.load(images[ i ], { load: true });
+    }
+};
